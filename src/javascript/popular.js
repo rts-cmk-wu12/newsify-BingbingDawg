749aaba763bc079d24d037ec9ba11a09e90ed5ac
BingbingDawg <u87960@rts-365.dk>
2024-11-22 23:54:40 +0100

    Put popular articles into containers

--- src/javascript/popular.js
+++ src/javascript/popular.js
@@ -1,11 +1,12 @@
 require ('../scss/popular.scss')
-const collapseNewsArray = ["Health", "Sport", "Travel", "Business", "Europe"];
-function collapseMenu(titleData){
+function collapseMenu(titleData, articleTitle, articleAbstract ){
   const collapseButton = document.createElement("button")
   const collapseDiv = document.createElement("div")
   const collapseLogo = document.createElement("img")
   const collapseContentContainer = document.createElement("div")
   const collapseTitle = document.createElement("h2")
+  const collapseContainerTitle = document.createElement("h3")
+  const collapseContainerText = document.createElement("p")
 
   collapseButton.classList.add("collapsible")
   collapseDiv.classList.add("newsarticle-category")
@@ -15,6 +16,9 @@ function collapseMenu(titleData){
   collapseButton.append(collapseDiv)
   collapseDiv.append(collapseLogo, collapseTitle)
   collapseTitle.innerText = titleData
+  collapseContainerTitle.innerText = articleTitle; 
+  collapseContainerText.innerText = articleAbstract;
+  collapseContentContainer.append(collapseContainerTitle, collapseContainerText )
 
   collapseButton.addEventListener('click', function() {
     collapseButton.classList.toggle('active');
@@ -24,16 +28,14 @@ function collapseMenu(titleData){
     } else {
         collapseContentContainer.style.display = "block"
     }
+
 });
 
 const mainElement = document.querySelector("main")
 
 mainElement.append(collapseButton, collapseContentContainer)
-
-
 }
 
-collapseNewsArray.forEach(category => collapseMenu(category));
 
 // fetch 
 const { fetchMostPopularByViews } = require ('./nyt');
@@ -41,8 +43,14 @@ const { fetchMostPopularByViews } = require ('./nyt');
 (async () => {
   try { const popularNews = await fetchMostPopularByViews(7);
     console.log(popularNews);
+
+    const addedCategories = {};
+
     popularNews.forEach(article => {
-        console.log(article.section)
+      if (!addedCategories[article.section]) {
+        collapseMenu(article.section, article.title, article.abstract);
+        addedCategories[article.section] = true;
+      }
     })
 } catch (error) {
     console.error('Error', error);
